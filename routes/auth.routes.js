cbea516251a63932aa37f0b54d7ef7147650b56f
linaSlh <lyna.slh@gmail.com>
2024-04-06 00:45:03 +0200

    isauth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 // routes/auth.routes.js
-
+//require("../middleware/jwt.middleware");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 const express = require("express");
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
@@ -123,15 +124,15 @@ router.post('/login', (req, res, next) => {
  
  
 // GET  /auth/verify
-// router.get('/verify', isAuthenticated, (req, res, next) => {       // <== CREATE NEW ROUTE
+ router.get('/verify', isAuthenticated, (req, res, next) => {       // <== CREATE NEW ROUTE
  
 //     // If JWT token is valid the payload gets decoded by the
 //     // isAuthenticated middleware and made available on `req.payload`
-//     console.log(`req.payload`, req.payload);
+     console.log(`req.payload`, req.payload);
    
 //     // Send back the object with user data
 //     // previously set as the token payload
-//     res.status(200).json(req.payload);
-  // });
+     res.status(200).json(req.payload);
+ });
 
 module.exports = router;
