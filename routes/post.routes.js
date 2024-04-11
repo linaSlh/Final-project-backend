b4a66112a5076bc08a9896c04bbb14773e5e3f3e
linaSlh <lyna.slh@gmail.com>
2024-04-11 17:35:35 +0200

    ch

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,19 +1,16 @@
-
- // module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
 const post = require("../models/Post.model");
 const comment = require("../models/Comment.model");
-const User = require("../models/User.model")
+const User = require("../models/User.model");
 
-//  POST /api/posts  -  Creates a new post
+// POST /api/posts - Creates a new post
 router.post("/posts", (req, res, next) => {
-  const { title, content,region} = req.body;
-// console.log(req.payload._id);
-  post.create({ title, content, region, author:req.payload._id, })
+  const { title, content, region } = req.body;
+  const { _id: author } = req.payload; // Assuming req.payload contains the user information
+  post.create({ title, content, region, author })
     .then((response) => res.json(response))
     .catch((err) => {
       console.log("Error while creating the post", err);
@@ -21,18 +18,14 @@ router.post("/posts", (req, res, next) => {
     });
 });
 
-
-// var Aposts = [];
-// var upost =[];
-// var name = [];
+// GET /api/posts - Retrieves all posts
 router.get("/posts", async (req, res, next) => {
   try {
     const allposts = await post.find().populate("author");
-    const Apo = allposts.map(async (post) => {
+    const Ap = await Promise.all(allposts.map(async (post) => {
       const user = await User.findById(post.author);
       return { ...post.toObject(), user: user.username };
-    });
-    const Ap = await Promise.all(Apo);
+    }));
     res.json(Ap);
   } catch (err) {
     console.log("Error while getting the posts", err);
@@ -40,124 +33,29 @@ router.get("/posts", async (req, res, next) => {
   }
 });
 
- 
-
-
-
-
- 
-
-
-
-
-// })
-// .catch((err) => {
-//   console.log("Error while getting the postss", err);
-//   res.status(500).json({ message: "Error while getting the posts" });
-// });
-
-
-
-
-// //  GET /api/posts -  Retrieves all of the posts
-// router.get("/posts", (req, res, next) => {
-//   post.find()
-//     // .populate("comment")
-//     .then((allposts) => res.json(allposts))
-//     .catch((err) => {
-//       console.log("Error while getting the postss", err);
-//       res.status(500).json({ message: "Error while getting the posts" });
-//     });
-// });
-
-
-
-//  GET /api/posts/:postId -  Retrieves a specific post by id
-router.get("/posts/:postId",(req, res, next) => {
+// GET /api/posts/:postId - Retrieves a specific post by id
+router.get("/posts/:postId", (req, res, next) => {
   const { postId } = req.params;
-
   if (!mongoose.Types.ObjectId.isValid(postId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
   post.findById(postId)
-  .populate('author')
-  .then((post) => res.status(200).json(post))
-.catch((err) => {
-   console.log("Error while retrieving the author", err);
-   res.status(500).json({ message: "Error while retrieving the Author" });
- });
+    .populate("author")
+    .then((post) => res.status(200).json(post))
+    .catch((err) => {
+      console.log("Error while retrieving the post", err);
+      res.status(500).json({ message: "Error while retrieving the post" });
+    });
 });
-// Each post document has `tasks` array holding `_id`s of Task documents
-// We use .populate() method to get swap the `_id`s for the actual Task documents
 
- 
-  // try {
-  //   // Retrieve the post
-  //   const retrievedPost = await post.findById(postId);
-    
-  //   if (!retrievedPost) {
-  //     res.status(404).json({ message: "Post not found" });
-  //     return;
-  //   }
-
-  //   // Fetch the author's user ID from the retrieved post
-  //   const authorId = retrievedPost.author;
-
-  //   // Retrieve the user document using the author ID
-  //   const author = await User.findById(authorId);
-    
-  //   if (!author) {
-  //     res.status(404).json({ message: "Author not found" });
-  //     return;
-  //   }
-
-  //   // Extract the username from the author document
-  //   const username = author.username;
-
-  //   // Include the author's username in the post object
-  //   const postWithAuthor = { ...retrievedPost.toObject(), author: username };
-    
-    // Send the response with the post containing the author's username
-//     res.status(200).json(postWithAuthor);
-//   } catch (err) {
-//     console.log("Error while retrieving the post", err);
-//     res.status(500).json({ message: "Error while retrieving the post" });
-//   }
-// });
-
-
-
-
-
-
-
-
-//   post.findById(postId)
-//   // .populate({
-//   //   path: 'author',
-//   //   select: 'username' // Select only the 'username' field from the referenced User document
-//   // })
-//     .then((post) => res.status(200).json(post))
-//     .catch((err) => {
-//       console.log("Error while retrieving the post", err);
-//       res.status(500).json({ message: "Error while retrieving the post" });
-//     });
-// });
-
-
-
-
-
-// PUT  /api/posts/:postId  -  Updates a specific post by id
+// PUT /api/posts/:postId - Updates a specific post by id
 router.put("/posts/:postId", (req, res, next) => {
   const { postId } = req.params;
-
   if (!mongoose.Types.ObjectId.isValid(postId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
-
   post.findByIdAndUpdate(postId, req.body, { new: true })
     .then((updatedPost) => res.json(updatedPost))
     .catch((err) => {
@@ -166,21 +64,15 @@ router.put("/posts/:postId", (req, res, next) => {
     });
 });
 
-// DELETE  /api/posts/:postId  -  Deletes a specific post by id
+// DELETE /api/posts/:postId - Deletes a specific post by id
 router.delete("/posts/:postId", (req, res, next) => {
   const { postId } = req.params;
-
   if (!mongoose.Types.ObjectId.isValid(postId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
-
   post.findOneAndDelete({ _id: postId })
-    .then(() =>
-      res.json({
-        message: `post with ${postId} is removed successfully.`,
-      })
-    )
+    .then(() => res.json({ message: `post with ${postId} is removed successfully.` }))
     .catch((err) => {
       console.log("Error while deleting the post", err);
       res.status(500).json({ message: "Error while deleting the post" });
