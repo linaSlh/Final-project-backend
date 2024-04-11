e5c1b696795520ab91c8f4833408f222674a8d0d
linaSlh <lyna.slh@gmail.com>
2024-04-11 14:43:05 +0200

    undo username display

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -23,26 +23,26 @@ router.post("/posts", (req, res, next) => {
 });
 
 
-var Aposts = [];
-var upost =[];
-var name = [];
-router.get("/posts", (req, res, next) => {
-  post.find()
-    .populate("author")
-    .then((allposts) =>
-     {  console.log(allposts);
-        let posts = allposts;
-        posts.map( async (post) => {
+// var Aposts = [];
+// var upost =[];
+// var name = [];
+// router.get("/posts", (req, res, next) => {
+//   post.find()
+//     .populate("author")
+//     .then((allposts) =>
+//      {  console.log(allposts);
+//         let posts = allposts;
+//         posts.map( async (post) => {
         
 	//	let tmp = await User.findById(post.author);
 		// let nom = await User.findById(post.author);
 		// name.push(nom.username);
-		Aposts.push(post);
-  });
+// 		Aposts.push(post);
+//   });
 
-})
-  whenDone(res);
-})
+// })
+//   whenDone(res);
+// })
 
 // .catch((err) => {
 //   console.log("Error while getting the postss", err);
@@ -52,18 +52,23 @@ router.get("/posts", (req, res, next) => {
 
 
 
-  function whenDone(res) {
-    //you should be able to access fixedItems here
-	for ( i = 0; i < name.length; i++) { 
-	Aposts[i].user = name[i];
-	}
+  // function whenDone(res) {
+  //   //you should be able to access fixedItems here
+	// for ( i = 0; i < name.length; i++) { 
+	// Aposts[i].user = name[i];
+	// }
+
+
+
 //	console.log(Aposts);
 //	console.log(upost);
-	console.log(Aposts[5]);
-    res.json(Aposts);
 
-	console.log(name.length , Aposts.length);
-  }
+
+// console.log(Aposts[5]);
+//     res.json(Aposts);
+
+// 	console.log(name.length , Aposts.length);
+//   }
 
 
 
@@ -92,66 +97,76 @@ router.get("/posts", (req, res, next) => {
 
 
 //  GET /api/posts/:postId -  Retrieves a specific post by id
-router.get("/posts/:postId",(req, res, next) => {
+router.get("/posts/:postId",async(req, res, next) => {
   const { postId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(postId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
-  User.findById(Author)
-  .then((post) => {
-         res.status(200).json(User);
-        console.log(User);
-        })
-.catch((err) => {
-   console.log("Error while retrieving the author", err);
-   res.status(500).json({ message: "Error while retrieving the Author" });
- });
+
+
+//   User.findById(Author)
+//   .then((post) => {
+//          res.status(200).json(User);
+//         console.log(User);
+//         })
+// .catch((err) => {
+//    console.log("Error while retrieving the author", err);
+//    res.status(500).json({ message: "Error while retrieving the Author" });
+//  });
+
+
+
 // Each post document has `tasks` array holding `_id`s of Task documents
 // We use .populate() method to get swap the `_id`s for the actual Task documents
-post.findById(postId)
- // .populate("comments")
- .then((post) => res.status(200).json(post))
- .catch((err) => {
-   console.log("Error while retrieving the post", err);
-   res.status(500).json({ message: "Error while retrieving the post" });
- });
-});
+
+
+
+// post.findById(postId)
+//  // .populate("comments")
+//  .then((post) => res.status(200).json(post))
+//  .catch((err) => {
+//    console.log("Error while retrieving the post", err);
+//    res.status(500).json({ message: "Error while retrieving the post" });
+//  });
+// });
  
-  // try {
+
+  try {
   //   // Retrieve the post
-  //   const retrievedPost = await post.findById(postId);
+    const retrievedPost = await post.findById(postId);
     
-  //   if (!retrievedPost) {
-  //     res.status(404).json({ message: "Post not found" });
-  //     return;
-  //   }
+    if (!retrievedPost) {
+      res.status(404).json({ message: "Post not found" });
+      return;
+    }
 
   //   // Fetch the author's user ID from the retrieved post
-  //   const authorId = retrievedPost.author;
+    const authorId = retrievedPost.author;
 
   //   // Retrieve the user document using the author ID
-  //   const author = await User.findById(authorId);
+    const author = await User.findById(authorId);
     
-  //   if (!author) {
-  //     res.status(404).json({ message: "Author not found" });
-  //     return;
-  //   }
+
+    if (!author) {
+      res.status(404).json({ message: "Author not found" });
+      return;
+    }
 
   //   // Extract the username from the author document
-  //   const username = author.username;
+    const username = author.username;
 
   //   // Include the author's username in the post object
-  //   const postWithAuthor = { ...retrievedPost.toObject(), author: username };
+    const postWithAuthor = { ...retrievedPost.toObject(), author: username };
     
     // Send the response with the post containing the author's username
-//     res.status(200).json(postWithAuthor);
-//   } catch (err) {
-//     console.log("Error while retrieving the post", err);
-//     res.status(500).json({ message: "Error while retrieving the post" });
-//   }
-// });
+    res.status(200).json(postWithAuthor);
+  } catch (err) {
+    console.log("Error while retrieving the post", err);
+    res.status(500).json({ message: "Error while retrieving the post" });
+  }
+});
 
 
 
@@ -184,6 +199,9 @@ router.put("/posts/:postId", (req, res, next) => {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
+
+
+// Update The post 
 
   post.findByIdAndUpdate(postId, req.body, { new: true })
     .then((updatedPost) => res.json(updatedPost))
